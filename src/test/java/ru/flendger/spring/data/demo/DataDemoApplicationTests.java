4f3146a45f78f224493dc404a4411b5e461cba71
flendger <kiru.ee@yandex.ru>
2020-12-25 15:39:00 +0300

    initial commit

---
+++ src/test/java/ru/flendger/spring/data/demo/DataDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.flendger.spring.data.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
